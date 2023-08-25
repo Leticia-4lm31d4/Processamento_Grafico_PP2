3a2ef5b73aedd79f7b6a54894093bb9b3fbd13f0
Leticia Almeida <leticiapaulinoferreira@gmail.com>
2023-08-25 00:55:32 -0300

    Configurando ambiente WebGl

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,23 @@
-/* código JavaScript principal */
+// Configurando o Ambiente WebGL 
+
+// Cria uma cena
+const scene = new THREE.Scene();
+
+// Cria uma câmera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Cria um renderizador
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Inicia um loop de renderização
+function animate() {
+  requestAnimationFrame(animate);
+  
+  // Atualize sua cena aqui
+
+  renderer.render(scene, camera);
+}
+animate();
